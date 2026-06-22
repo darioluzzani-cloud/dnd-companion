ed0a39b73ae3f65ffcf4da7a7a5cbf5254a2c826
darioluzzani-cloud <darioluzzani@gmail.com>
2026-06-22 14:27:57 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'velmora-v1';
+const SHELL = [
+  '/velmora',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Le chiamate a Supabase vanno sempre in rete
+  if (e.request.url.includes('supabase')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        // Salva in cache le risorse statiche
+        if (res.ok && e.request.method === 'GET') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
